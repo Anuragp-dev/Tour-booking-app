7920edca89411ada3723d4e6ac5c7beb9cd62e4f
ANURAG P <anuragp212@gmail.com>
2024-07-16 10:47:25 +0530

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,7 +1,7 @@
 import {createContext, useEffect, useReducer} from 'react';
 
 
-// inital state localstrage
+// inital state localstorage
 const initial_state = {
     user:localStorage.getItem('user') !== undefined ? JSON.parse(localStorage.getItem('user')) :null,
     loading:false,
