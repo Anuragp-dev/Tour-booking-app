b50009df026fad133357d637f028b397e3861763
ANURAG P <anuragp212@gmail.com>
2024-07-21 23:37:54 +0530

    Update verifyToken.js

--- backend/Utils/verifyToken.js
+++ backend/Utils/verifyToken.js
@@ -1,7 +1,7 @@
 import jwt  from "jsonwebtoken";
 
 
-// store the token in cookies 
+// store the token in cookies
 const verifyToken = (req, res, next) => {
     const token = req.cookies.accessToken
 
