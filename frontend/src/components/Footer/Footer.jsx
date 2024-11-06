a947811b282e6eac39f0ed22f07a46c9d213b66d
ANURAG P <anuragp212@gmail.com>
2024-11-06 09:56:50 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './footer.css'; 
 import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/images/logo.png';
+import logo from '../../assets/images/logo.png'; 
 
 
 // add quick link   
